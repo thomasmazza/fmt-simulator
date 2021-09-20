ccadd0bfd9e81d84230fe254ec3365b28e4ccbf8
Lasse Alsmeyer <lasse.alsmeyer@rwth-aachen.de>
2021-09-20 14:33:43 +0200

    This commit adds the base component class, including a test program. The installation of boost has to be done locally in the same CLionProjects folder just like the project folder.

--- components/component.hpp
+++ components/component.hpp
@@ -19,4 +19,4 @@ namespace component {
     };
 }
 
-#include "/src/component.cpp"
+#include "src/component.cpp"
--- components/test.cpp
+++ components/test.cpp
@@ -5,13 +5,15 @@
 #include<iostream>
 
 #include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/io.hpp>
 #include "component.hpp"
 
 using namespace boost::numeric::ublas;
 
 int main(int argc, char *argv[]){
-    vector<double> vec(argv[1]);
-    component::Component<double> testComp = component::Component(&vec);
-    std::cout << "Result Test " << testComp.getPos() << std::endl;
+    vector<double> vec(std::stoi(argv[1]));
+    for(int i=0; i<vec.size(); i++) vec(i)=i;
+    component::Component<double> testComp(vec);
+    std::cout << "Test vector " << testComp.getPos() << std::endl;
     return 0;
 };
