b9cb06a8af475cfda82e94df42d894eefa41ef2a
Niklas D <niklasdamhorst@web.de>
2021-09-21 18:35:10 +0200

    This commit adds minor fixes to mirror.

--- components/src/mirror.cpp
+++ components/src/mirror.cpp
@@ -2,7 +2,6 @@
 #include <boost/numeric/ublas/vector.hpp>
 #include <boost/geometry/arithmetic/cross_product.hpp>
 
-
 typedef typename boost::numeric::ublas::vector<double> vector;
 
 Mirror::Mirror(vector& _pos, vector& _normal):Mirror::Component(_pos, _normal){
