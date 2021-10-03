35e0bbff706d1501ee213eafe712f57482353cb1
nikobark <nikolay.panov@protonmail.com>
2021-10-04 02:08:10 +0300

    This commit adds the Detector class. Still missing a few functions;

--- components/include/detector.hpp
+++ components/include/detector.hpp
@@ -5,15 +5,19 @@
 #include "component.hpp"
 #include "../../photon/include/photon.hpp"
 
+
 typedef typename boost::numeric::ublas::vector<double> vector;
+typedef typename boost::numeric::ublas::vector<*Photon> photonVector;//Kann verändert sein, abhängig davon wie die Photon zuerst gespeichert werden
+typedef typename boost::numeric::ublas::matrix<photonVector> photonMatrix;
 
 class Detector: public Component {
 protected:
     int size;
     double pixelSize;
     double length;
+    photonMatrix sensor;
 public:
-    Detector(vector&, vector&, int, double);
-    bool hitComponent(Photon&);
+    Detector(vector&, vector&, int, double, double);
+    //bool hitComponent(Photon&);
     vector getInPoint(Photon&);
 };
