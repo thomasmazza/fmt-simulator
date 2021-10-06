5b12416189f8097b0bce0cc53003b5b8e861b6a1
Thomas Mazza <thomas.mazza@rwth-aachen.de>
2021-10-06 22:58:16 +0200

    Correct Syntax

--- components/include/lensOneSided.hpp
+++ components/include/lensOneSided.hpp
@@ -1,14 +1,14 @@
 #pragma once
 
-#include <boost/numeric/ublas/vector.hpp>
 #include "lens.hpp"
+#include "utils.hpp"
 
-typedef typename boost::numeric::ublas::vector<double> vector;
 
 class LensOneSided: public Lens {
 private:
     double radiusW;
     bool planeIsFront;
 public:
-    LensOneSided(vector&, vector&, double, double, double, bool);
+    LensOneSided(vector&, vector&, double, double, double, double, bool);
+    bool getOutDir(Photon&);
 };
