f75e9df42b6c7b2db598897cd9040c8337b0d721
nikobark <nikolay.panov@protonmail.com>
2021-10-11 17:31:06 +0300

    This commit adds image creation, wavelength to rgb convert and some changes to utils.cpp

--- components/src/utils.cpp
+++ components/src/utils.cpp
@@ -7,19 +7,19 @@ void cross_product(vector& result,vector& a, vector& b){
     result(2)=a(0)*b(1)-a(1)*b(0);
 }
 
-void normalize(vector& a) {
-    double t = sqrt(a(0)^2 + a(1)^2 + a(2)^2);
-    a(0) = a(0) / t;
-    a(1) = a(1) / t;
-    a(2) = a(2) / t;
-}
-
 vector cross_product_2(vector& a, vector& b) {
     vector result(3);
     result(0)=a(1)*b(2)-a(2)*b(2);
     result(1)=a(2)*b(0)-a(0)*b(2);
     result(2)=a(0)*b(1)-a(1)*b(0);
     return result;
+}
+
+void normalizeVector(vector& a) {
+    double t = sqrt(a(0)^2 + a(1)^2 + a(2)^2);
+    a(0) = a(0) / t;
+    a(1) = a(1) / t;
+    a(2) = a(2) / t;
 }
 
 double dot_product(vector& a, vector& b) {
