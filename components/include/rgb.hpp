632154c7c3be941b6f25864091088086ba7159b4
nikobark <nikolay.panov@protonmail.com>
2021-10-12 20:27:26 +0300

    This commit adds RGB brightness adjustments in createImage();

--- components/include/rgb.hpp
+++ components/include/rgb.hpp
@@ -5,8 +5,11 @@ public:
     double r;
     double g;
     double b;
+    double intensity;
     void setRGB(double &, double &, double &);
     RGB();
-    RGB(double &, double &, double &);
+    RGB(double &, double &, double &, double &);
     RGB(const RGB&); //Kopierkonstruktor
 };
+
+
--- components/src/rgb.cpp
+++ components/src/rgb.cpp
@@ -6,16 +6,18 @@ void RGB::setRGB(double & _r, double & _g, double & _b) {
     b = _b;
 }
 
-RGB::RGB() : r(0), g(0), b(0) {}
+RGB::RGB() : r(0), g(0), b(0), intensity(0) {}
 
-RGB::RGB(double & _r, double & _g, double & _b) {
+RGB::RGB(double & _r, double & _g, double & _b, double & _i) {
     r = _r;
     g = _g;
     b = _b;
+    intensity = _i;
 }
 
 RGB::RGB(const RGB & obj)  {
     r = obj.r;
     g = obj.g;
     b = obj.b;
+    intensity = obj.intensity;
 }
