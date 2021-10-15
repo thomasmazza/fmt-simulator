536a6e0376d317f7768b65e6ec2d484bb0489e4d
Niklas-Laptop <niklasdamhorst@web.de>
2021-10-15 15:34:32 +0200

    This commit fixes bugs in Layout and changes names of the functions in mirror.

--- components/src/mirrorCircle.cpp
+++ components/src/mirrorCircle.cpp
@@ -5,5 +5,4 @@ std::string MirrorCircle::getType() {
 }
 
 MirrorCircle::MirrorCircle(vector& _pos, vector& _normal, double _radius):MirrorElliptical(vector& pos, vector& _normal, double _radius, double _radius) {
-    //TODO: Nothing
 }
