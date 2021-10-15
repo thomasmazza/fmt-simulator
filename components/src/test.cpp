30fd5266e518887a18941074794b64f3f220ad5a
nikobark <nikolay.panov@protonmail.com>
2021-10-15 13:25:37 +0300

    Adds test.cpp

--- components/src/test.cpp
+++ components/src/test.cpp
@@ -1,27 +1,53 @@
 #include<iostream>
 
 #include <boost/numeric/ublas/vector.hpp>
+#include <boost/numeric/ublas/matrix.hpp>
+#include <vector>
 #include <boost/numeric/ublas/io.hpp>
-#include "component.hpp"
-#include "filter.hpp"
-#include "mirror.hpp"
-#include "mirrorElliptical.hpp"
+#include "../include/component.hpp"
+//#include "filter.hpp"
+//#include "mirror.hpp"
+#include "../include/detector.hpp"
+
+//#include "mirrorElliptical.hpp"
 #include "../../photon/include/photon.hpp"
-#include "mirrorRectangle.hpp"
+//#include "mirrorRectangle.hpp"
 
-int main(int argc, char *argv[]) {
-    vector testVector(3);
-    for (unsigned i = 0; i < testVector.size(); i++) {
-        testVector(i) = i;
+typedef typename boost::numeric::ublas::vector<double> vector;
+
+int main() {
+    vector v (3);
+    for (int i = 0; i < 3; i++) {
+        v(i) = 0;
     }
-    Filter filter1(testVector, testVector, 1, 1);
-    std::cout << filter1.getLowerLimit() << '\n';
-    Component component1(testVector, testVector);
-    vector test = component1.getPosition();
-    //MirrorRectangle lb = MirrorRectangle(testVector, testVector, 1, 1);
-    std::cout << test << '\n';
-    MirrorElliptical la = MirrorElliptical(testVector, testVector, 1, 1);
-    Photon testPhoton(testVector, testVector, 600, 500);
-    std::cout << la.hitComponent(testPhoton, testVector) << '\n';
+    Photon ph(v, v, 700, 0);
+    std::vector<Photon> phV;
+    for (int i = 0; i < 10; i++) {
+        for (int j = 0; j < 10; j++) {
+            v(0) = j / 10.0;
+            v(1) = i / 10.0;
+            v(2) = 0;
+            ph.setPosition(v);
+            v(0) = 0;
+            v(1) = 0;
+            v(2) = 1;
+            ph.setDirection(v);
+            phV.push_back(ph);
+        }
+    }
 
-};
+    v(0) = 0.5;
+    v(1) = 0.5;
+    v(2) = 5;
+
+    vector vn(3);
+    vn(0) = 0;
+    vn(1) = 0;
+    vn(2) = 5;
+
+
+    Detector d(v, vn, 50, 0.03);
+
+    return 0;
+
+}
