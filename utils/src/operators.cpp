fa1fd09a98a679fe3d0ccdca8dc8534eef82dbd3
JamesDoe13 <niklasdamhorst@web.de>
2021-10-19 09:29:53 +0000

    This commit fixes minor bugs in the operator.cpp (size of the vectors).

--- utils/src/operators.cpp
+++ utils/src/operators.cpp
@@ -3,24 +3,24 @@
 
 std::vector<double> operator+(const std::vector<double>& vector1, const std::vector<double>& vector2){
     assert(vector1.size()==vector2.size());
-    std::vector<double> vector;
+    std::vector<double> vector(vector1.size());
     for(unsigned int i = 0;i<vector1.size();i++){
-        vector = vector1 + vector2;
+        vector[i] = vector1[i] + vector2[i];
     }
     return vector;
 }
 
 std::vector<double> operator-(const std::vector<double>& vector1, const std::vector<double>& vector2){
     assert(vector1.size()==vector2.size());
-    std::vector<double> vector;
+    std::vector<double> vector(vector1.size());
     for(unsigned int i = 0;i<vector1.size();i++){
-        vector = vector1 - vector2;
+        vector[i] = vector1[i] - vector2[i];
     }
     return vector;
 }
 
 std::vector<double> operator-(const std::vector<double>& _vector){
-    std::vector<double> vector;
+    std::vector<double> vector(vector.size());
     for(unsigned int i = 0; i < _vector.size(); i++){
         vector[i] = -_vector[i];
     }
