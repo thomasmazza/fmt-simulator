79988b4beb1108b03e04472e9a15fb5d8738431a
nikobark <nikolay.panov@protonmail.com>
2021-10-20 11:46:01 +0300

    This commit adds sharpness and brightness as members in detector, and a method to calculate depthOfField in Utils; Minor adjustments and improvements in detector.cpp;

--- components/src/detector.cpp
+++ components/src/detector.cpp
@@ -98,6 +98,23 @@ void Detector::getInPoint(Photon &photon) {
 }
 
 bmp_vector Detector::createImage() {
+    double max = sensor[0][0].intensity;
+    double min = max;
+    double avg = 0.0;
+    for (int i = 0; i < size; i++) {
+        for (int j = 0; j < size; j++) {
+            if (sensor[i][j].intensity > max) {
+                max = sensor[i][j].intensity;
+            } else if (sensor[i][j].intensity < min) {
+                min = sensor[i][j].intensity;
+            }
+            avg += sensor[i][j].intensity;
+        }
+    }
+    avg = avg / (size * size);
+    brightness = (avg / max) * 100;
+
+
     rgb_vector image(size * size);
     for (int i = 0; i < size; i++) {
         for (int j = 0; j < size; j++) {
