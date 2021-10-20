b5776d01410b1ef32c7de4659356d14e68a51e59
Thomas Mazza <thomas.mazza@rwth-aachen.de>
2021-10-20 14:33:59 +0200

    Copy constuctor for lens

--- components/src/lens.cpp
+++ components/src/lens.cpp
@@ -3,6 +3,8 @@
 Lens::Lens(std::vector<double>& _pos, std::vector<double>& _normal, double _radiusH,double _n, double _d, ComponentType _type):Component(_pos, _normal, _type), radiusH(_radiusH), n(_n), d(_d){
 }
 
+Lens::Lens(const Lens &lens): Component(lens), radiusH(lens.radiusH), d(lens.d), n(lens.n){}
+
 const double &Lens::getD() {
     return d;
 }
